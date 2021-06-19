8fac60d72fc2f5fd937c30977149ff6fac4a972f
Tevin Dirks <tevinchaddirks@gmail.com>
2021-06-19 14:31:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+$surface = $('.surface');
+$car = $('.car');
+$img = $('.car img');
+let flag = true;
+
+const cars = ['img/img_05.png','img/img_06.png' ];
+
+
+$(document).on('keypress', function(e){
+if(e.which == 13){
+$($surface).toggleClass('moveRight');
+$($car).toggleClass('suspension');
+}
+})
+
+$(document).on('keypress', function(e){
+if(e.which ==119){
+if(flag){
+flag = false;
+$img.attr('src', cars[0]);
+}else{
+flag = true;
+$img.attr('src', cars[1]);
+}
+}
+})
+
+});
